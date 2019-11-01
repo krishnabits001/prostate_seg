75a335c383b83af75405ed151ee177dce8a5f201
krishnch <krishna.chaitanya@gess.ethz.ch>
2019-11-01 16:58:25 +0100

    train and test for GE scanner data

--- code/train_model/train_script.sh
+++ code/train_model/train_script.sh
@@ -1,1 +1,6 @@
-python tr_unet_baseline.py --no_of_tr_imgs=tr18 --comb_tr_imgs=c1 --lr_seg=0.001 --ver=0
+#For medical decatholon challenge data - public data
+#python tr_unet_baseline.py --dataset=prostate_md --no_of_tr_imgs=tr18 --comb_tr_imgs=c1 --lr_seg=0.001 --ver=0
+
+#For GE scanner private data
+python tr_unet_baseline.py --dataset=prostate_ge --no_of_tr_imgs=tr6 --comb_tr_imgs=c1 --lr_seg=0.001 --ver=0
+
